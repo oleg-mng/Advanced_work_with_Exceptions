9c36dd1b8c046b37e7e9bc9350fb8c7f5bc023dc
oleg <legik7127@gmail.com>
2023-04-12 12:00:55 +0600

    added class MyException

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -4,29 +4,44 @@ public class Program {
     public static void main(String[] args) {
         doIt();
     }
-    static void doIt(){
+
+    static void doIt() {
         try {
             InnerApp.foo();
-        }
-        catch (ArithmeticException e){
+        } catch (ArithmeticException e) {
             System.out.println("Неверная работа с числом");
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Неверный ввод/вывод");
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Что то неверно");
 
         }
     }
 }
-class InnerApp{
-    static void foo() throws Exception{
-        int num = bar();
-        throw new Exception("Проблема не с числом");
-    }
-    static int bar() throws Exception{
-        throw new Exception("Проблема с числом");
 
+class InnerApp {
+    static void foo() throws MyException {
+        try {
+            int num = bar();
+        } catch (MyException e) {
+            if (e.a == 11){
+
+            }
+        }
+
+    }
+
+    static int bar() throws MyException {
+        var ex = new MyException("Проблема с числом");
+        ex.a = 20123;
+        throw ex;
+    }
+}
+
+class MyException extends Exception {
+    int a;
+
+    public MyException(String msg) {
+        super(msg);
     }
 }
